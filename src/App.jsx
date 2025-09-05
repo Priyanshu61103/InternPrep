fee83a6e07e8a10b00514b83f5992813ec1e74e2
RohanPathak2004 <rohanpathak258@gamil.com>
2025-09-05 23:44:49 +0530

    Resume checker is created two other tabs remaining

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,25 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import HomePage from "./Components/HomePage";
 import User from "./Pages/LoginAndRegis/User.jsx";
+import Main from "./Pages/CareerResourcesPage/MainPage/Main.jsx";
 const App = () => {
   return (
+      <div>
+
     <Provider store={store}>
       <div>
+
          <Router>
           <Routes>
+
             <Route path="/" element={<HomePage/>}></Route>
               <Route path="/userL-R" element={<User/>}></Route>
+              <Route path='/carrer_resources' element={<Main/>}></Route>
           </Routes>
          </Router>
       </div>
     </Provider>
+      </div>
   );
 };
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,49 +1,29 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import { useSelector , useDispatch } from "react-redux";
-import { changeMode } from "../Redux/slice/slice";
+import React from 'react'
+import {NavLink} from "react-router-dom";
+
 const Navbar = () => {
-  const mode = useSelector((state) => state.mode.value);  
-  const dispatch = useDispatch();
-
   return (
-    <div className={mode==="light" ? "h-20 w-full bg-white flex items-center justify-around fixed top-0 left-0 right-0 z-30":"h-20 w-full bg-gray-950 flex items-center justify-around fixed top-0 left-0 right-0 z-30"}>
-      <img src="../intern_prep_logo.png" alt="" className="h-15 w-60" />
+    <div className="h-20 w-full bg-white flex items-center justify-around">
+        <img src="../intern_prep_logo.jpeg" alt="" className="h-15 w-60"/>
 
-      <div className={mode==="light"?"w-150 flex text-xl font-semibold gap-x-7 text-gray-950":"w-150 flex text-xl font-semibold gap-x-7 text-white"}>
-        <button className="navbarOption h-12 w-17 p-2 rounded-full">
-          Home
-        </button>
-        <button className="navbarOption h-12 w-30 p-2 rounded-full">
-          Internships
-        </button>
-        <button className="navbarOption h-12 w-44 p-2 rounded-full">
-          Career Resources
-        </button>
-        <button className="navbarOption h-12 w-27 p-2 rounded-full">
-          About Us
-        </button>
-      </div>
-      <div>
-        <input
-          type="text"
-          placeholder="Search..."
-          className={mode==="light"?"h-10 w-50 p-4 border-2 bg-gray-200 border-gray-400 outline-0 rounded-full placeholder:text-gray-950":"h-10 w-50 p-4 border-2 bg-gray-800 border-gray-400 outline-0 rounded-full placeholder:text-white"}
-        />
-      </div>
+        <div className="w-150 flex justify-between text-xl font-semibold ">
+            <div>Home</div>
+            <div>Internships</div>
+            <NavLink to='/carrer_resources'>
+            <div>Career Resources</div>
+            </NavLink>
+            <div>About Us</div>
+        </div>
 
-      <div>
-          <button className="h-10 w-10 rounded-full bg-gray-300 flex justify-center items-center shadow-xl/10"><img src="light-mode.png" alt="" className="h-5 w-5" onClick={()=>dispatch(changeMode())}/></button>
-      </div>
-      <div>
-        <NavLink to="/userL-R">
-          <button className="signInButton h-15 w-50 bg-blue-950 text-white p-5 rounded-2xl font-semibold cursor-pointer">
-            Sign In/Register
-          </button>
-        </NavLink>
-      </div>
+        <div>
+            <NavLink to='/userL-R'>
+            <button className="h-15 w-50 bg-gray-900 text-white p-5 rounded-xl font-semibold cursor-pointer">
+                Sign In/Register
+            </button>
+            </NavLink>
+        </div>
     </div>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
